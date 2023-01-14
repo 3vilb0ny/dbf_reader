2311c34630f02cd95133394146817184251b5154
Ignacio Cabrera <ignaciocabrera1224@gmail.com>
2023-01-14 14:20:00 -0300

    feat: :sparkles: Add suport to other datatypes

--- lib/src/cell_structure.dart
+++ lib/src/cell_structure.dart
@@ -27,11 +27,21 @@ class CellStructure {
   }
 
   String _translateDataType() {
-    // C	Character, padded with spaces if shorter than the field length
-    // D	Date stored as string in YYYYMMDD format
-    // F	Floating point number, stored as string, padded with spaces if shorter than the field length
-    // N	Floating point number, stored as string, padded with spaces if shorter than the field length
-    // L	Boolean value, stored as one of YyNnTtFf. May be set to ? if not initialized
+    /**
+     * https://www.dbase.com/Knowledgebase/INT/db7_file_fmt.htm
+     * 
+     * C	Character, padded with spaces if shorter than the field length
+     * D	Date stored as string in YYYYMMDD format
+     * F	Floating point number, stored as string, padded with spaces if shorter than the field length
+     * N	Floating point number, stored as string, padded with spaces if shorter than the field length
+     * L	Boolean value, stored as one of YyNnTtFf. May be set to ? if not initialized
+     * M  Memo, a string - 10 digits (bytes) representing a .DBT block number. The number is stored as a string, right justified and padded with blanks
+     * @  Timestamp - The date is the number of days since  01/01/4713 BC. Time is hours * 3600000L + minutes * 60000L + Seconds * 1000L
+     * l  Long
+     * +  Long autoincrement
+     * O  Double
+     * G  OLE
+     */
     switch (dataType) {
       case "C":
         return "Char";
@@ -43,6 +53,18 @@ class CellStructure {
         return "Double";
       case "L":
         return "bool";
+      case "M":
+        return "String";
+      case "@":
+        return "Double";
+      case "l":
+        return "Double";
+      case "+":
+        return "Double";
+      case "O":
+        return "Double";
+      case "G":
+        return "OLE";
       default:
         return "Unrecognized";
     }
